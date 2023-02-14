5a4f0080f8aed6142fe13b7853d67dc1aba02815
S K Bhaskar <bhasker.skb@gmail.com>
2023-02-14 18:15:36 +0530

    Class variable example 2

--- Class_variables.py
+++ Class_variables.py
@@ -1,11 +1,12 @@
 class Employee:
     raise_amount = 1.04  # class variables  It can be used across the Class Methods
-    reduce_amout = 0.88  # class variables
+    num_of_emps = 0
 
     def __init__(self, first: str, last: str, pay: int):
         self.first = first
         self.last = last
         self.pay = pay
+        Employee.num_of_emps += 1
 
     def fullname(self):
         return "{} {} ".format(self.first, self.last)
@@ -16,13 +17,8 @@ class Employee:
     def reduce_pay(self):
         self.pay = int(self.pay * Employee.reduce_amout)
 
-
+print(Employee.num_of_emps)
 emp1 = Employee('Bhaskar', 'S K', 50000)
 emp2 = Employee('Udhay', 'Pt', 60000)
 
-print(emp1.pay)
-emp1.pay_raise()
-print(emp1.pay)
-emp1.reduce_pay()
-print(emp1.pay)
-
+print(Employee.num_of_emps)
